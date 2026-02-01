16f96992cc9268952719527a3f4b6d1e0f703ffb
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-02-01 20:25:48 +0900

    feat: 플레이어 상태 중앙 관리 시스템 구현

---
+++ Assets/01_Scripts/Player/PlayerStateManager.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateManager : MonoBehaviour
+{
+    public bool isAttacking { get; set; } = false;
+    public bool isLockedOn { get; set; } = false;
+
+    public GameObject targetEnemy { get; set; }
+    public float targetDistance { get; set; }
+
+    public Animator animator { get; set; }
+    public CharacterController characterController { get; set; }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+}
