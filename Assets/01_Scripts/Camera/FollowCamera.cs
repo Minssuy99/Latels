76de138cf227b57cb0778f4cda29768b1c4dedad
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-02-01 20:19:41 +0900

    feat: 플레이어 추적 카메라 구현

---
+++ Assets/01_Scripts/Camera/FollowCamera.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private Vector3 cameraOffset;
+    private GameObject _player;
+
+    private void Start()
+    {
+        _player = GameObject.FindWithTag("Player");
+
+        if (_player == null)
+        {
+            Debug.Log("플레이어의 태그를 가진 오브젝트가 없습니다.");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_player)
+            return;
+
+        transform.position = _player.transform.position + cameraOffset;
+    }
+}
