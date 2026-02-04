e35ae9da38123bbf6fd942e0b1e8a490e6456588
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-02-04 15:16:52 +0900

    feat: DangerZone 감지

---
+++ Assets/01_Scripts/Player/DodgeDetector.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeDetector : MonoBehaviour
+{
+    public bool isEnemyAttackNearby { get; private set; } = false;
+
+    [SerializeField] private float detectionDuration = 0.3f;
+    private float detectionTimer = 0f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("EnemyDangerZone"))
+        {
+            detectionTimer = detectionDuration;
+        }
+    }
+
+    private void Update()
+    {
+        if (detectionTimer > 0)
+        {
+            isEnemyAttackNearby = true;
+            detectionTimer -= Time.deltaTime;
+        }
+        else
+        {
+            isEnemyAttackNearby = false;
+        }
+    }
+}
