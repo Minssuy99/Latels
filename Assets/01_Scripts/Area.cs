e63ac35f845cdda3de9f991888cba7696632a2a6
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-02-07 17:27:23 +0900

    feat: 입구/출구 결계 시스템, 보스UI 활성화, RemoveEnemy

--- Assets/01_Scripts/Area.cs
+++ Assets/01_Scripts/Area.cs
@@ -6,27 +6,62 @@ public class Area : MonoBehaviour
 {
     private List<EnemyStateManager> enemies = new List<EnemyStateManager>();
 
+    [SerializeField] private GameObject bossUI;
+    [SerializeField] private GameObject Entrance;
+    [SerializeField] private GameObject Exit;
+
     private void Start()
     {
         for (int i = 0; i < transform.childCount; i++)
         {
             EnemyStateManager enemy = transform.GetChild(i).GetComponent<EnemyStateManager>();
-            enemies.Add(enemy);
+            if (enemy != null)
+                enemies.Add(enemy);
         }
 
         Debug.Log($"Enemy : " +  enemies.Count);
+
+        Entrance.SetActive(false);
+        Exit.SetActive(true);
     }
 
     public List<EnemyStateManager> GetEnemies() => enemies;
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log($"OnTriggerEnter: {other.name}");
         if (other.CompareTag("Player"))
         {
+            if(bossUI != null)
+                bossUI.SetActive(true);
+
+            Entrance.SetActive(true);
             foreach(EnemyStateManager enemy in enemies)
             {
-                enemy.Activate();
+                enemy.Activate(this);
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (enemies.Count == 0)
+            {
+                Exit.SetActive(true);
+            }
+        }
+    }
+
+    public void RemoveEnemy(EnemyStateManager enemy)
+    {
+        enemies.Remove(enemy);
+        Debug.Log("남은 적 수 : " + enemies.Count);
+
+        if (enemies.Count == 0)
+        {
+            Exit.SetActive(false);
         }
     }
 }
