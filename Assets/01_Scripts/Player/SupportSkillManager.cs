bf075aa428906e8bb0c0159bae74b86b725c0369
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-02-19 04:51:27 +0900

    feat: 지원캐릭터 스킬 발동 및 쿨타임 관리 구현

---
+++ Assets/01_Scripts/Player/SupportSkillManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SupportSkillManager : MonoBehaviour
+{
+    /* 지원캐릭터 1 설정 */
+    private GameObject SubCharacter1;
+    public float SubSkill_1_CoolTime = 15f;
+    public float remain_SubSkill_1_CoolTime;
+    private bool canSubSkill_1 = true;
+
+    private PlayerStateManager player;
+    private SupportCharacter support1;
+
+    private void Awake()
+    {
+        player = GetComponent<PlayerStateManager>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Warmup());
+    }
+
+    private void Update()
+    {
+        if (canSubSkill_1 == false)
+        {
+            remain_SubSkill_1_CoolTime -= Time.unscaledDeltaTime;
+
+            if (remain_SubSkill_1_CoolTime <= 0)
+                canSubSkill_1 = true;
+        }
+    }
+
+    public void OnSubSkill_1(InputValue value)
+    {
+        if (player.IsDead) return;
+        if (!canSubSkill_1) return;
+        if (player.attack.GetEnemies().Count == 0) return;
+
+        canSubSkill_1 = false;
+        remain_SubSkill_1_CoolTime = SubSkill_1_CoolTime;
+
+        SubCharacter1.transform.position = player.transform.position;
+        SubCharacter1.SetActive(true);
+        support1.Initialize(player.attack.GetEnemies());
+    }
+
+    IEnumerator Warmup()
+    {
+        yield return null;
+        if (SubCharacter1 == null) yield break;
+        SubCharacter1.SetActive(true);
+        yield return null;
+        SubCharacter1.SetActive(false);
+    }
+
+    public void SetSupport(GameObject support)
+    {
+        SubCharacter1 = support;
+        support1 = support.GetComponent<SupportCharacter>();
+    }
+}
