1740b2525bbe7ff744aac82b937ac0477761c837
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-02-19 04:49:37 +0900

    feat: 클리어 연출 시퀀스 구현

---
+++ Assets/01_Scripts/Stage/StageClear.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+using DG.Tweening;
+using System.Collections;
+
+public class StageClear : MonoBehaviour
+{
+    [Header("UI Settings")]
+    [SerializeField] private GameObject inGameUI;
+    [SerializeField] private CanvasGroup fadeImage;
+    [SerializeField] private RectTransform fadeLeft;
+    [SerializeField] private RectTransform fadeRight;
+    [SerializeField] private RectTransform doorLeft;
+    [SerializeField] private RectTransform doorRight;
+
+    [SerializeField] private RectTransform clockImage;
+    [SerializeField] private RectTransform clockLeft;
+    [SerializeField] private RectTransform clockRight;
+    [SerializeField] private float duration = 0.5f;
+
+    [Header("클리어 UI")]
+    [SerializeField] private VictoryUI victoryUI;
+
+    private Transform startPoint;
+    private Transform endPoint;
+
+    public void SetCameraPoint(Transform cameraStartPoint, Transform cameraEndPoint)
+    {
+        startPoint = cameraStartPoint;
+        endPoint =  cameraEndPoint;
+    }
+
+    private void Start()
+    {
+        StageManager.Instance.OnStageClear += StartClearCoroutine;
+
+        float doorHeight = clockLeft.transform.parent.GetComponent<RectTransform>().rect.height;
+        clockLeft.sizeDelta = new Vector2(doorHeight, 0f);
+        clockRight.sizeDelta = new Vector2(doorHeight, 0f);
+
+        clockLeft.anchoredPosition = Vector2.zero;
+        clockRight.anchoredPosition = Vector2.zero;
+
+        fadeImage.alpha = 0f;
+        doorLeft.gameObject.SetActive(false);
+        doorRight.gameObject.SetActive(false);
+        clockLeft.gameObject.SetActive(false);
+        clockRight.gameObject.SetActive(false);
+        clockImage.gameObject.SetActive(false);
+    }
+
+    private void StartClearCoroutine()
+    {
+        StartCoroutine(ClearSequence());
+    }
+
+    IEnumerator ClearSequence()
+    {
+        BulletTimeManager.Instance.playerSlowDown = true;
+        Time.timeScale = 0.1f;
+        Time.fixedDeltaTime = 0.02f * 0.1f;
+        yield return new WaitForSecondsRealtime(1f);
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = 0.02f;
+        BulletTimeManager.Instance.playerSlowDown = false;
+        yield return new WaitForSecondsRealtime(1f);
+
+        inGameUI.SetActive(false);
+        yield return new WaitForSecondsRealtime(1f);
+        Time.timeScale = 0f;
+
+        fadeImage.DOFade(1f, 0.75f).SetUpdate(true);
+        yield return new WaitForSecondsRealtime(1.5f);
+
+        Time.timeScale = 1f;
+        doorLeft.gameObject.SetActive(true);
+        doorRight.gameObject.SetActive(true);
+        clockImage.gameObject.SetActive(true);
+        fadeLeft.DOAnchorPosX(-fadeLeft.rect.width, duration);
+        fadeRight.DOAnchorPosX(fadeRight.rect.width, duration);
+
+        yield return new WaitForSecondsRealtime(1f);
+        clockImage.DORotate(new Vector3(0, 0, 360), 1.5f, RotateMode.FastBeyond360).OnComplete(() =>
+        {
+            clockImage.gameObject.SetActive(false);
+            clockLeft.gameObject.SetActive(true);
+            clockRight.gameObject.SetActive(true);
+        });
+        yield return new WaitForSecondsRealtime(1.75f);
+        Camera.main.GetComponent<FollowCamera>().enabled = false;
+        Camera.main.transform.position = startPoint.position;
+        Camera.main.transform.rotation = startPoint.rotation;
+
+        doorLeft.DOAnchorPosX(-doorLeft.rect.width, duration);
+        doorRight.DOAnchorPosX(doorRight.rect.width, duration);
+        yield return new WaitForSecondsRealtime(2f);
+
+        Camera.main.transform.DOMove(endPoint.position, 0.7f);
+        Camera.main.transform.DORotate(endPoint.rotation.eulerAngles, 0.7f);
+        yield return new WaitForSecondsRealtime(1.5f);
+
+        victoryUI.Show("The Latels of the Cataclysm", 4635, 6000);
+    }
+}
