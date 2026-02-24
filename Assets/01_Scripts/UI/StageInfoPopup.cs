5569f4c4e438601aaf4429f1f0b2be03aa92b3a7
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-02-24 10:14:38 +0900

    feat: 팝업 애니메이션 추가

--- Assets/01_Scripts/UI/StageInfoPopup.cs
+++ Assets/01_Scripts/UI/StageInfoPopup.cs
@@ -1,16 +1,38 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class StageInfoPopup : MonoBehaviour
 {
+    [SerializeField] private RectTransform infoPanel;
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        infoPanel = infoPanel.GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void Start()
+    {
+        canvasGroup.alpha = 0;
+    }
+
     public void ShowStageInfoPopup(int index)
     {
+        infoPanel.anchoredPosition = new Vector2(1000, 0);
         GameManager.Instance.stageData = GameManager.Instance.chapterData.stages[index];
         gameObject.SetActive(true);
+        infoPanel.DOAnchorPosX(0, 0.25f);
+        canvasGroup.DOFade(1, 0.25f);
     }
 
     public void HideStageInfoPopup()
     {
-        gameObject.SetActive(false);
+        canvasGroup.DOFade(0, 0.25f);
+        infoPanel.DOAnchorPosX(1000, 0.25f).OnComplete(() =>
+        {
+            gameObject.SetActive(false);
+        });
     }
 
     public void GoToSelectScreen()
