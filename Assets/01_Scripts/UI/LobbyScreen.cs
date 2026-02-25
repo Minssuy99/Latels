f8091a26fd9dfcdc43ddb87b660fd0982279bd17
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-02-25 15:17:32 +0900

    refactor: LobbyManager 를 LobbyScreen 으로 전환

--- Assets/01_Scripts/UI/LobbyScreen.cs
+++ Assets/01_Scripts/UI/LobbyScreen.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using DG.Tweening;
 
@@ -7,30 +8,25 @@ public enum HideDirection
     Right,
 }
 
-[System.Serializable]
+[Serializable]
 public class UIElement
 {
     public RectTransform rect;
     public HideDirection hideDirection;
 }
 
-public class LobbyManager : Singleton<LobbyManager>
+public class LobbyScreen : UIScreen
 {
-    [Header("References")]
-    public GameObject LobbyScreen;
-    public ChapterScreen chapterScreen;
-    public StageScreen stageScreen;
-    public StageInfoPopup stageInfoPopup;
-    public CharacterSelectScreen characterSelectScreen;
-
-    [Header("Fader Settings")]
+    [Header("※ UI Animation")]
     [SerializeField] private UIElement[] elements;
-    [SerializeField] private GameObject backgroundPanel;
     [SerializeField] private float hideOffset;
     [SerializeField] private float duration;
 
-    private bool isHidden = false;
-    private bool isFullScreen = false;
+    [Space(10)]
+    [Header("※ Background")]
+    [SerializeField] private GameObject backgroundPanel;
+
+    private bool isHidden;
     private Vector2[] originalPositions;
     private CanvasGroup[] canvasGroups;
 
@@ -45,32 +41,36 @@ public class LobbyManager : Singleton<LobbyManager>
             canvasGroups[i] = elements[i].rect.GetComponent<CanvasGroup>();
         }
 
-        LobbyScreen.SetActive(true);
-        chapterScreen.gameObject.SetActive(false);
-        stageScreen.gameObject.SetActive(false);
-        stageInfoPopup.gameObject.SetActive(false);
         backgroundPanel.SetActive(false);
-        characterSelectScreen.gameObject.SetActive(false);
+    }
 
-        GameManager.Instance.onReturnToLobby?.Invoke();
-        GameManager.Instance.onReturnToLobby = null;
+    public override void OnEnter(Action onComplete)
+    {
+        gameObject.SetActive(true);
+        ShowLobbyUI();
+        onComplete?.Invoke();
+    }
+
+    public override void OnExit(Action onComplete)
+    {
+        HideLobbyUI();
+        gameObject.SetActive(false);
+        onComplete?.Invoke();
     }
 
     public void EnterFullScreen()
     {
         HideLobbyUI();
-        isFullScreen = true;
         backgroundPanel.SetActive(true);
     }
 
     public void ExitFullScreen()
     {
         ShowLobbyUI();
-        isFullScreen = false;
         backgroundPanel.SetActive(false);
     }
 
-    public void HideLobbyUI()
+    private void HideLobbyUI()
     {
         if (isHidden) return;
         isHidden = true;
@@ -82,20 +82,20 @@ public class LobbyManager : Singleton<LobbyManager>
             direction = elements[i].hideDirection == HideDirection.Left ? -1 : 1;
             targetX = originalPositions[i].x + direction * hideOffset;
 
-            elements[i].rect.DOAnchorPosX(targetX, duration).SetEase(Ease.InOutQuart) ;
-            canvasGroups[i].DOFade(0f, duration).SetEase(Ease.InOutQuart) ;
+            elements[i].rect.DOAnchorPosX(targetX, duration).SetEase(Ease.InOutQuart);
+            canvasGroups[i].DOFade(0f, duration).SetEase(Ease.InOutQuart);
         }
     }
 
-    public void ShowLobbyUI()
+    private void ShowLobbyUI()
     {
         if (!isHidden) return;
         isHidden = false;
 
         for (int i = 0; i < elements.Length; i++)
         {
-            elements[i].rect.DOAnchorPos(originalPositions[i], duration).SetEase(Ease.InOutQuart) ;
-            canvasGroups[i].DOFade(1f, duration).SetEase(Ease.InOutQuart) ;
+            elements[i].rect.DOAnchorPos(originalPositions[i], duration).SetEase(Ease.InOutQuart);
+            canvasGroups[i].DOFade(1f, duration).SetEase(Ease.InOutQuart);
         }
     }
 }
