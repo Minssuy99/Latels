04a783ad9b9a4612c587617fe83ec46eebf19d7a
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-02-25 15:21:03 +0900

    feat: 전체화면 암전 페이드 추가 및 페이드 중복 실행 개선

--- Assets/01_Scripts/Manager/FadeManager.cs
+++ Assets/01_Scripts/Manager/FadeManager.cs
@@ -10,10 +10,11 @@ public enum FadeDirection
 
 public class FadeManager : Singleton<FadeManager>
 {
+    [SerializeField] private CanvasGroup blackFadePanel;
+
     [SerializeField] private GameObject fader;
     [SerializeField] private float halftoneOffset;
     [SerializeField] private float duration;
-    public float Duration => duration;
 
     [SerializeField] private RectTransform clockImage;
     [SerializeField] private float clockSpeed;
@@ -21,7 +22,6 @@ public class FadeManager : Singleton<FadeManager>
     private RectTransform faderRect;
     private CanvasGroup canvasGroup;
     private float screenWidth;
-    private bool isPlaying;
 
     private float clockRotation;
     private Tween clockTween;
@@ -33,17 +33,34 @@ public class FadeManager : Singleton<FadeManager>
         faderRect = fader.GetComponent<RectTransform>();
         faderRect.anchoredPosition = new Vector2(screenWidth + halftoneOffset, 0);
         canvasGroup.alpha = 1;
+        blackFadePanel.alpha = 0;
+        blackFadePanel.gameObject.SetActive(false);
         fader.SetActive(false);
+    }
+
+    public void PlayBlackFade(Action onScreenCovered, float holdTime)
+    {
+        blackFadePanel.gameObject.SetActive(true);
+        blackFadePanel.alpha = 0f;
+        blackFadePanel.DOFade(1, 0.25f).OnComplete(() =>
+        {
+            onScreenCovered?.Invoke();
+            blackFadePanel.DOFade(0, 0.25f).SetDelay(holdTime).OnComplete(() =>
+            {
+                blackFadePanel.gameObject.SetActive(false);
+            });
+        });
     }
 
     public void PlayFade(FadeDirection direction,Action onScreenCovered, float holdTime)
     {
-        if (isPlaying) return;
-        isPlaying = true;
+        faderRect.DOKill();
+        clockTween?.Kill();
+        DOTween.Kill(this);
 
         fader.SetActive(true);
 
-        float rotateDir = (direction == FadeDirection.RightToLeft) ? 360f : -360f;
+        float rotateDir = direction == FadeDirection.RightToLeft ? 360f : -360f;
         float Speed = (direction == FadeDirection.RightToLeft) ? clockSpeed : clockSpeed / 3f;
         float totalOffset = screenWidth + halftoneOffset;
         float startX, endX;
@@ -78,11 +95,10 @@ public class FadeManager : Singleton<FadeManager>
                 {
                     faderRect.DOAnchorPosX(endX, duration).SetEase(Ease.InOutQuint).OnComplete(() =>
                     {
-                        isPlaying = false;
                         fader.SetActive(false);
                     });
-                });
-            });
+                }).SetId(this);
+            }).SetId(this);
         });
     }
 }
