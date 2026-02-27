23890d3a33340ffed007e56c4cfb836761cf9761
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-02-27 14:15:47 +0900

    feat: 테스트씬 부트스트랩 추가

---
+++ Assets/01_Scripts/Debug/TestBootstrap.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TestBootstrap : MonoBehaviour
+{
+    [Header("[Player Settings]")]
+    [SerializeField] private CharacterData playerData;
+    [SerializeField] CharacterStats playerStatsOverride;
+
+    [Space(10)]
+    [Header("[Enemy Settings]")]
+    [SerializeField] private EnemyData enemyData;
+    [SerializeField] EnemyStats enemyStatsOverride;
+    [SerializeField] private Transform[] spawnPoints;
+
+    [Space(10)]
+    [Header("Runtime Test")]
+    public CharacterData runtimePlayerData;
+    public EnemyData runtimeEnemyData;
+
+    private GameObject playerObj;
+    private List<EnemyStateManager> enemies = new();
+    private void Start()
+    {
+        playerObj = Instantiate(playerData.battlePrefab);
+        CharacterData playerDataCopy = Instantiate(playerData);
+        playerDataCopy.stats = playerStatsOverride;
+        runtimePlayerData = playerDataCopy;
+
+        playerObj.GetComponent<CharacterSetup>().SetRole(CharacterRole.Main, playerDataCopy);
+        playerObj.tag = "Player";
+        playerObj.name = "Player";
+
+        TimeManager.Instance.SetAnimator(playerObj.GetComponent<Animator>());
+        InGameUIManager.Instance.SetPlayer(playerObj.GetComponent<PlayerStateManager>());
+        Camera.main.GetComponent<FollowCamera>().SetPlayer(playerObj.transform);
+
+        EnemyData enemyDataCopy = Instantiate(enemyData);
+        enemyDataCopy.stats = enemyStatsOverride;
+        runtimeEnemyData = enemyDataCopy;
+
+        foreach (Transform point in spawnPoints)
+        {
+            GameObject enemyObj = Instantiate(enemyData.prefab, point.position, point.rotation);
+            EnemyStateManager enemy = enemyObj.GetComponent<EnemyStateManager>();
+            enemy.name = "Enemy";
+            enemy.SetData(enemyDataCopy);
+            enemy.SetPlayer(playerObj);
+            enemy.Activate(null);
+            enemies.Add(enemy);
+        }
+
+        playerObj.GetComponent<PlayerAttack>().SetEnemies(enemies);
+    }
+}
