94c3b3227862146305388526b795fc9c2071323b
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-02-28 15:08:52 +0900

    delete: PlayerSkill → PlayerMainSKill/SupportSkill 분리 (이전 파일 삭제)

--- Assets/01_Scripts/Character/RuruneAttack.cs
+++
@@ -1,8 +0,0 @@
-public class RuruneAttack : PlayerAttack, IBattleComponent
-{
-    public void Shoot()
-    {
-        if (player.targetEnemy == null) return;
-        player.targetEnemy.attack.TakeDamage(player.CharacterData.stats.damage);
-    }
-}
