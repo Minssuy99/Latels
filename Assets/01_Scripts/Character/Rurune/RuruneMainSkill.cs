844ab1e06423bc7e76c6fab1428d19055f1e94e0
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-03-01 17:23:21 +0900

    feat: 루루네 메인스킬 구현

--- Assets/01_Scripts/Character/Rurune/RuruneMainSkill.cs
+++ Assets/01_Scripts/Character/Rurune/RuruneMainSkill.cs
@@ -3,55 +3,95 @@ using System.Collections;
 
 public class RuruneMainSkill : PlayerMainSkill
 {
-    private RuruneAttack ruruneAttack;
+    [SerializeField] private float range;
+    [SerializeField] private int effectCount = 3;
+    [SerializeField] private GameObject[] muzzleEffects;
+    [SerializeField] private GameObject[] explosionEffects;
+
+    private RuruneAttack attack;
+    private Collider[] skillHitBuffer = new Collider[20];
+
+    protected override void Awake()
+    {
+        base.Awake();
+        attack = GetComponent<RuruneAttack>();
+    }
 
     private IEnumerator SkillSequence()
     {
         player.animator.updateMode = AnimatorUpdateMode.UnscaledTime;
-        GameObject target1 = targetDetector.FindNearestTarget()?.gameObject;
+        GameObject target = targetDetector.FindNearestTarget()?.gameObject;
 
-        if (player.move.moveDirection.sqrMagnitude > 0)
-            transform.rotation = Quaternion.LookRotation(player.move.moveDirection);
-        else if (target1)
-            transform.LookAt(target1.transform);
+        if (player.move.MoveDirection.sqrMagnitude > 0)
+            transform.rotation = Quaternion.LookRotation(player.move.MoveDirection);
+        else if (target)
+            transform.LookAt(target.transform);
 
-        yield return new WaitForSecondsRealtime(0.1f);
-        player.animator.SetTrigger("Kick1");
-        yield return new WaitForSecondsRealtime(0.8f);
+        yield return new WaitForSecondsRealtime(0.9f);
 
-        GameObject target2 = targetDetector.FindNearestTarget(target1)?.gameObject;
-        if (!target2 && target1 && target1.GetComponent<EnemyStateManager>().health.HP > 0)
-        {
-            target2 = target1;
-        }
-
-        if (player.move.moveDirection.sqrMagnitude > 0)
-            transform.rotation = Quaternion.LookRotation(player.move.moveDirection);
-        else if (target2)
-            transform.LookAt(target2.transform);
-
-        yield return new WaitForSecondsRealtime(0.1f);
-        player.animator.SetTrigger("Kick2");
-        yield return new WaitForSecondsRealtime(1.675f);
-        player.animator.updateMode = AnimatorUpdateMode.Normal;
+        target = targetDetector.FindNearestTarget()?.gameObject;
+        if (player.move.MoveDirection.sqrMagnitude > 0)
+            transform.rotation = Quaternion.LookRotation(player.move.MoveDirection);
+        else if (target)
+            transform.LookAt(target.transform);
     }
 
     public void OnRuruneSkill()
     {
-        if (ruruneAttack == null)
-            ruruneAttack = GetComponent<RuruneAttack>();
-        ruruneAttack.Shoot();
+        int count = Physics.OverlapSphereNonAlloc(transform.position, range, skillHitBuffer);
+        for (int i = 0; i < count; i++)
+        {
+            EnemyStateManager enemy = skillHitBuffer[i].GetComponent<EnemyStateManager>();
+            if (enemy == null) continue;
+            if (enemy.currentState is EnemyDeadState) continue;
+
+            enemy.GetComponent<IDamageable>().TakeDamage(player.CharacterData.stats.skillDamage, transform.position);
+        }
+
+        for (int i = 0; i < effectCount; i++)
+        {
+            int explosionIndex = Random.Range(0, explosionEffects.Length);
+            GameObject explosionEffect = PoolManager.Instance.Get(explosionEffects[explosionIndex]);
+            Vector2 randomPos = Random.insideUnitCircle * range;
+            explosionEffect.transform.position = new Vector3(transform.position.x + randomPos.x, transform.position.y + Vector3.up.y, transform.position.z + randomPos.y);
+            explosionEffect.GetComponent<ParticleSystem>().Play();
+            PoolManager.Instance.Return(explosionEffect, 2f);
+        }
+
+        foreach (var muzzlePos in attack.MuzzlePosition)
+        {
+            int muzzleIndex = Random.Range(0, muzzleEffects.Length);
+            GameObject muzzleEffect = PoolManager.Instance.Get(muzzleEffects[muzzleIndex]);
+            muzzleEffect.transform.position = muzzlePos.position;
+            muzzleEffect.GetComponent<ParticleSystem>().Play();
+            PoolManager.Instance.Return(muzzleEffect, 2f);
+        }
     }
 
     public override void OnSkillStart()
     {
         base.OnSkillStart();
-        StartCoroutine(Run());
+        StartCoroutine(SkillSequence());
     }
 
-    private IEnumerator Run()
+    public void OnSkillEnd()
     {
-        yield return StartCoroutine(SkillSequence());
+        player.animator.updateMode = AnimatorUpdateMode.Normal;
+
+        if (player.dash.ConsumeDashBuffer())
+        {
+            player.animator.Play(AnimHash.Dash, 0, 0);
+            player.ChangeState(player.dashState);
+            return;
+        }
+
+        if (player.isLockedOn)
+            player.animator.Play(AnimHash.LockOn, 0, 0);
+        else if (player.move.MoveDirection.sqrMagnitude > 0)
+            player.animator.Play(AnimHash.Run, 0, 0);
+        else
+            player.animator.Play(AnimHash.Idle, 0, 0);
+
         EndSkill();
     }
 }
