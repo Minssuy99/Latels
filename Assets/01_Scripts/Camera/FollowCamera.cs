8cdb76ef91bd2116240aa8acc87393949bd5e1d3
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-03-03 01:31:45 +0900

    feat: FollowCamera 줌인/줌아웃 기능 추가

--- Assets/01_Scripts/Camera/FollowCamera.cs
+++ Assets/01_Scripts/Camera/FollowCamera.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
+using System.Collections;
+using DG.Tweening;
 
 public class FollowCamera : MonoBehaviour
 {
-    [SerializeField] private Vector3 cameraOffset;
+    [SerializeField] private Vector3 cameraOffset = new Vector3(0f, 6f, -7f);
     private Transform player;
+    private Vector3 originOffset;
+    private float zoomRatio = 1f;
+
+    private void Start()
+    {
+        originOffset = cameraOffset;
+    }
 
     public void SetPlayer(Transform player)
     {
@@ -14,6 +23,22 @@ public class FollowCamera : MonoBehaviour
     {
         if (!player) return;
 
-        transform.position = player.position + cameraOffset;
+        transform.position = player.position + originOffset * zoomRatio;
+    }
+
+    public void ZoomIn(float targetRadio, float duration = 0, Ease ease = Ease.OutQuad)
+    {
+        StartCoroutine(ZoomSequence(targetRadio, duration, ease));
+    }
+
+    public void ZoomOut(float duration = 0f, Ease ease = Ease.OutQuad)
+    {
+        StartCoroutine(ZoomSequence(1f, duration, ease));
+    }
+
+    private IEnumerator ZoomSequence(float targetRatio, float duration, Ease ease = Ease.OutQuad)
+    {
+        DOTween.To(() => zoomRatio, x => zoomRatio = x, targetRatio, duration).SetUpdate(true).SetEase(ease);
+        yield return new WaitForSeconds(duration);
     }
 }
